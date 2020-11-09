3557763009056b093c9025d11cf3bfcecdb25c03
tk-gomez <74206657+tk-gomez@users.noreply.github.com>
2020-11-09 12:59:14 -0600

    Update heladeria.h

--- heladeria.h
+++ heladeria.h
@@ -1,6 +1,18 @@
 #ifndef HELADERIA_H_INCLUDED
 #define HELADERIA_H_INCLUDED
 
+typedef struct {
+int numCliente;
+int pedido;
+float total;
+} Cliente;
+
+typedef struct {
+int numProductos;
+char sabor;
+char ingredientes;
+float precio;
+} Heladería;
 
 
 #endif // HELADERIA_H_INCLUDED
