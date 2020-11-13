2c992ae7219d4223b79d60cf6a3eb1582025dad9
iramvfranco <74212458+iramvfranco@users.noreply.github.com>
2020-11-12 22:27:13 -0600

    Update main_heladeria.c

--- main_heladeria.c
+++ main_heladeria.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"Heladeria.h"
+
+int main(){
+    struct Inventario *arrInv;
+    int i;
+    arrInv=(struct Inventario *)calloc(ELEMENTOS,sizeof(struct Inventario));
+    llenarInventario(arrInv);
+
+    printf("\n\n=================INVENTARIO ================\n");
+    for (i= 0; i< ELEMENTOS; i++)
+        listarinventario(arrInv[i]);
+    return 0;
+}
 
