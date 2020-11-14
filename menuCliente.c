02c2c05a8dd17245d2ae605decc02da68a868731
Diana-sh <60989615+Diana-sh@users.noreply.github.com>
2020-11-14 02:58:55 -0600

    Update menuCliente.c

--- menuCliente.c
+++ menuCliente.c
@@ -2,18 +2,25 @@
 #include "menu.h"
 #include "cliente.h"
 #include "cola.h"
+#include "heladeria.h"
 
-#define MAX 3
+#define max 5
+
 
 int main() {
     int opcion;
 //crear las variables apuntador a la estructura de datos cola y a la estructura cliente
 	Cola *cola;
-	cola = crearCola(5);  //MAXIMO DE 5 ELEMENTOS
+	cola = crearCola(max);  //MAXIMO DE 5 ELEMENTOS/clientes
 	Cliente *unCliente;
+	
+	
+	struct Inventario *arrInv;
+	arrInv = (struct Inventario *)calloc(MAX, sizeof(struct Inventario));
+	
 
     do {
-        opcion = desplegarMenu("\n1)Ordenar producto \n2)Entregar Helado \n3)Listar pedidos\n4)Salir\nOpcion: ",4);
+        opcion = desplegarMenu("\nCLIENTE:\n1)Ordenar producto \n\nEMPLEADO \n2)Preparar Helado \n3)Entregar Helado \n4)Listar pedidos\n5)Salir\nOpcion: ",5);
         switch (opcion){
 
         case 1:
@@ -26,26 +33,29 @@ int main() {
                else
                 printf("No hay espacio en la Heladeria...");
             break;
-        case 2:
-            if (!validarVacio(*cola)){
-				*unCliente = borrar(cola);
-				printf("\n***Entrega de pedido: ***\n");
-				listarCliente(*unCliente);
-				printf("\nCLIENTES EN FILA DE ESPERA:\n");
-				listar(*cola);
-            }
-            else
-                printf("\nNo hay CLIENTES por atender...:) \n");
+        case 2:    //PILA DE ORDENES/HELADOS
+				llenarInventario(arrInv);
             break;
         case 3:
-				listar(*cola);
+	        	if (!validarVacio(*cola)){
+					*unCliente = borrar(cola);
+					printf("\t=====Entrega de pedido: =====\n");
+					listarCliente(*unCliente);
+					printf("\nCLIENTES EN FILA DE ESPERA:\n");
+					listar(*cola);
+	            }
+	            else
+	                printf("\nNo hay CLIENTES por atender...:) \n");
             break;
-
+        case 4:
+		        listar(*cola);
+            break;
+		case 5: //liberar memoria
+        	liberaMemoria(cola);
+        	break;
         }
-    } while (opcion != 4);
+    } while (opcion != 5);
 
     //LEBERAR MEMORIA
     return 0;
 }
-
-
